324226387ae0b811ca34c2d40fac6bdc69cc7e9c
Nivas138 <nivasganesan97@gmail.com>
2021-02-08 12:09:07 +0530

    Removed xml configs and added annotation based configs

---
+++ src/main/java/com/learningspring/configs/LearnConfig.java
@@ -0,0 +1,10 @@
+package com.learningspring.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.learningspring"})
+public class LearnConfig {
+
+}
